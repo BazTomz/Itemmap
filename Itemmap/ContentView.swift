d11ee21902e66a3935ed23cf62d76f7ee4dfbb1f
momoe <113basil.e@gmail.com>
2023-08-16 10:19:15 +0900

    test3

--- Itemmap/ContentView.swift
+++ Itemmap/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Image(systemName: "heart")
                 .imageScale(.large)
-                .foregroundColor(.yellow)
+                .foregroundColor(.green)
             Text("Hello, world!")
         }
         .padding()
